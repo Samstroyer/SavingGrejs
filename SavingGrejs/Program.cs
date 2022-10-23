084219114b2122a0bffa085a5189896215aba1e7
Samuel Palmér <samiskramis@gmail.com>
2022-10-23 10:59:49 +0200

    PC Transfer

--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -8,7 +8,23 @@ Game();
 
 void Game()
 {
+    Greet();
+    Store();
 }
+
+void Store()
+{
+    bool inStore = true;
+    while (inStore)
+    {
+        Console.WriteLine("Welcome to the Goose store!");
+        Console.WriteLine("Type 'help' to see available commands.");
+
+
+    }
+}
+
+
 
 void Greet()
 {
@@ -16,28 +32,40 @@ void Greet()
 
     if (CheckForFile())
     {
-        Console.WriteLine("Do you want to load your save? (Saved: {0})", File.GetLastAccessTime(fileName));
-        Console.WriteLine("(y/n)");
-
         bool completed = false;
 
         while (!completed)
         {
+
+            Console.WriteLine("Do you want to load your save? (Saved: {0})", File.GetLastAccessTime(fileName));
+            Console.WriteLine("(y/n)");
+
             string? answer = Console.ReadLine();
             if (answer?.ToLower() == "y")
             {
                 completed = true;
+                LoadStoreItems();
             }
             else if (answer?.ToLower() == "n")
             {
                 completed = true;
             }
+            Console.Clear();
         }
     }
     else
     {
         Console.WriteLine("No saves found! Press any key to continue.");
         Console.ReadLine();
+    }
+    Console.Clear();
+}
+
+void LoadStoreItems()
+{
+    foreach (var goose in JsonSerializer.Deserialize<List<Goose>>(fileName))
+    {
+        storeGooses.Add(goose);
     }
 }
 
